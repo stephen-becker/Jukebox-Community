f2ae30b7ca6ef75fd4bc07069ae3bdabf5298f04
Stephen Becker <9274335+stephen-becker@users.noreply.github.com>
2021-04-24 17:39:09 -0400

    Update Error Library.py

--- Initial/Error Library.py
+++ Initial/Error Library.py
@@ -1,1 +1,21 @@
+def lib_error(typeOfError):
 
+  if typeOfError == "0x0225a":
+    errorRtrn = "The inputed file could not be found, please type the file name again or check if it exists."
+
+  if typeOfError == "0x0225b":
+    errorRtrn = "The inputed file does not have a valid ending extension (.wav?)."
+
+  if typeOfError == "0x0224a":
+    errorRtrn = "Too many '/' in the entered string, did you correctly enter the path?"
+
+  if typeOfError == "0x0224b":
+    errorRtrn = "You entered something else besides 'Y' or 'N' try again"
+
+  if typeOfError == "0x0224d":
+    errorRtrn = "Directory (folder) does not exist."
+  
+  if typeOfError == "1x0001":
+    errorRtrn = "An internal error has occured."
+
+  return errorRtrn;
