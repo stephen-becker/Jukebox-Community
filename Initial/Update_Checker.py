1e0c9bfbddd540c81154bab7fc42debd4b10294b
Stephen Becker <9274335+stephen-becker@users.noreply.github.com>
2021-04-24 18:09:04 -0400

    Update Update_Checker.py

--- Initial/Update_Checker.py
+++ Initial/Update_Checker.py
@@ -1,1 +1,29 @@
+import sys
+from termcolor import colored, cprint
 
+def initalStageCommit(typePrint):
+  
+  if typePrint == 'gpuTxt':
+    gpuText = colored('Assigned GPU:', 'red')
+    print(gpuText)
+  elif typePrint == 'driveTxt':
+    driveText = colored('\nGoogle Drive Connector:', 'red')
+    print(driveText)
+  elif typePrint == 'updateTxt':
+    updateText = colored('\nChecking For Updates:', 'red')
+    print(updateText)
+  
+def versionChecker(vv):
+  convertedNum = '16.02'
+  recentBuild = float(convertedNum)
+  currentUse = vv
+  needsUpdate = False
+  goodVersion = colored('Build '+convertedNum, 'green')
+  
+  if currentUse == recentBuild:
+    print("You're running the beta version of this build.", goodVersion)
+    print("There might be errors during your session, report them on discord.")
+    needsUpdate = True
+  else:
+    exitTxt = colored('You\'re running an outdated version of this build.', 'red')
+    sys.exit(exitTxt)
