0e399115b9ab5be6322b6ce0009f9d5509cb0aa8
Stephen Becker <9274335+stephen-becker@users.noreply.github.com>
2021-05-12 19:22:01 -0400

    Update Basic_Func.py

--- Initial/Basic_Func.py
+++ Initial/Basic_Func.py
@@ -4,4 +4,4 @@ from termcolor import colored, cprint
 
 def ValidationHold():
   driveText = colored('\nSystem:', 'yellow')
-  print(driveText,"Please wait while the system scans and validates your entries. This may take a few seconds.")
+  print(driveText,"Please wait while the system scans and validates your entries. This may take a few seconds to one or two minutes.")
